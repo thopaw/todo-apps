52aafbe6ed6f468d1ccacba42d085ff5274a8073
Thomas Pawlitzki <thomas.pawlitzki@devk.de>
2017-12-03 21:22:42 +0100

    first version of simple app, shows some static todos

---
+++ server/app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+
+var todoRoutes = require('./routes/todo.js')
+var todoService = require('./todo/service.js')
+
+var port = process.env.PORT || 8000
+
+app.set('view engine', 'pug')
+app.set('views', './server/views/')
+
+// static routes
+app.use(express.static(__dirname + '/../node_modules/bootstrap/dist'))
+app.use(express.static(__dirname + '/../node_modules/jquery/dist'))
+
+app.get('/', function (req, res) {
+  res.render('index',{
+    'title': 'Todo App - NodeJS',
+    'heading': 'Nodo Application with NodeJS'
+  });
+});
+
+app.get('/todos', function (req, res) {
+  res.render('todos',{
+    'title': 'Todo App - NodeJS',
+    'heading': 'Nodo Application with NodeJS',
+    'todos': todoService.todos()
+  });
+});
+
+app.use('/api/todos', todoRoutes)
+
+app.listen(port, function () {
+  console.log('todo app listening on port ' + port);
+});
---
+++ server/todo/service.js
@@ -0,0 +1,14 @@
+module.exports = {
+  todos : function() {
+    return [
+      {
+        id: 1,
+        text: "Todo text #1"
+      },
+      {
+        id: 2,
+        text: "2nd Todo Text"
+      }
+    ]
+  }
+}

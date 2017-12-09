d937c03731b6862dee0f19b613b43523468873c4
Thomas Pawlitzki <thomas.pawlitzki@devk.de>
2017-12-09 11:25:41 +0100

    added livereload

--- README.md
+++ README.md
@@ -4,3 +4,11 @@ Simple Todo app implemented with
  - NodeJS
  - ExpressJS
  - Pug
+
+As development tools there is
+ - nodemon
+ - livereload
+ - concurrently (to run nodemon and livereload in parallel)
+
+# open issues
+ - browserify
--- server/app.js
+++ server/app.js
@@ -6,12 +6,18 @@ var todoService = require('./todo/service.js')
 
 var port = process.env.PORT || 8000
 
+// if(process.env.DEBUG_LIVE_RELOAD) {
+//   console.log("Starting Livereload Server")
+//   var livereload = require('./debug/livereload.js')
+// }
+
 app.set('view engine', 'pug')
 app.set('views', './server/views/')
 
 // static routes
 app.use(express.static(__dirname + '/../node_modules/bootstrap/dist'))
 app.use(express.static(__dirname + '/../node_modules/jquery/dist'))
+app.use(express.static(__dirname + '/public/'))
 
 app.get('/', function (req, res) {
   res.render('index',{
